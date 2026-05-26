20d2289ddf3de81ba4075a4989e0fffdbeea18f9
Jeac <jeff_20_06@hotmail.com>
2026-05-26 06:33:06 -0500

    Fix: Add Grupo entity to DocentesModule to resolve dependency injection error

--- backend/src/docentes/docentes.module.ts
+++ backend/src/docentes/docentes.module.ts
@@ -6,9 +6,11 @@ import { Docente } from "../entities/docente.entity";
 import { DocenteCurso } from "../entities/docente-curso.entity";
 import { Curso } from "../entities/curso.entity";
 import { Ambiente } from "../entities/ambiente.entity";
+import { CursoAmbiente } from "../entities/curso-ambiente.entity";
 import { HorarioAsignado } from "../entities/horario-asignado.entity";
 import { PeriodoAcademico } from "../entities/periodo-academico.entity";
 import { ParametrosCarga } from "../entities/parametros-carga.entity";
+import { Grupo } from "../entities/grupo.entity";
 import { DocentesService } from "./docentes.service";
 import { DocentesController } from "./docentes.controller";
 
@@ -20,9 +22,11 @@ import { DocentesController } from "./docentes.controller";
       DocenteCurso,
       Curso,
       Ambiente,
+      CursoAmbiente,
       HorarioAsignado,
       PeriodoAcademico,
       ParametrosCarga,
+      Grupo,
     ]),
     CacheModule.register(),
   ],

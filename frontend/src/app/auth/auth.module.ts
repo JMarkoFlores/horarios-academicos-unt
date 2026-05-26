08ba7983ff4a2291856837e9f4e0fd888c63e662
Jeac <jeff_20_06@hotmail.com>
2026-05-26 07:58:11 -0500

    Add TranslateModule to auth and layout modules to fix translate pipe errors

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { LoginComponent } from './login/login.component';
 import { LandingComponent } from './landing/landing.component';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [LoginComponent, LandingComponent],
-  imports: [SharedModule],
+  imports: [SharedModule, TranslateModule],
   exports: [LoginComponent, LandingComponent],
 })
 export class AuthModule {}
--- frontend/src/app/layout/layout.module.ts
+++ frontend/src/app/layout/layout.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { LayoutComponent } from './layout.component';
 import { SharedModule } from '../shared/shared.module';
 import { SharedDialogsModule } from '../shared/shared-dialogs.module';
 
 @NgModule({
   declarations: [LayoutComponent],
-  imports: [SharedModule, SharedDialogsModule],
+  imports: [SharedModule, SharedDialogsModule, TranslateModule],
   exports: [LayoutComponent],
 })
 export class LayoutModule {}
